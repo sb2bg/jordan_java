d9c8f087ac96b59c541f4015577c0d28f0e8df24
sb2bg <sullivanbognar@gmail.com>
2024-01-14 21:13:16 -0600

    Add SplashScreen widget

---
+++ lib/pages/splash.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToPage();
+  }
+
+  void navigateToPage() {
+    SharedPreferences.getInstance().then((value) {
+      String page = value.getString('default_screen') ?? '/home';
+      Navigator.pushReplacementNamed(context, page);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
